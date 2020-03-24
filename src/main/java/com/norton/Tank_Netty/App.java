948cbd98f9947535bcdc30773aa9ce1a656d6b06
NortonHua <1428370960@qq.com>
2020-03-24 22:56:43 +0800

    初始化项目，导入netty依赖

---
+++ src/main/java/com/norton/Tank_Netty/App.java
@@ -0,0 +1,13 @@
+package com.norton.Tank_Netty;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
